9a73a505744207c9d6929611bac5e902d78046e0
jansan <jansan-26@hotmail.com>
2019-10-22 16:07:34 +0200

    Components pages created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
 
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
 function App() {
-  return <div className="App">Hello App</div>;
+  return (
+    <div className="App">
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <Error />
+    </div>
+  );
 }
 
 export default App;
